17710f6151a3f66c90c807f7acf4a720bee40e36
1Dire <d01038145300@nate.com>
2023-11-03 02:38:51 +0900

    이름변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import LayoutComponent from "component/layoutComponent";
+import LayoutComponent from "component/LayoutComponent";
 import HeaderComponent from "component/HeaderComponent";
 function App() {
   return (
