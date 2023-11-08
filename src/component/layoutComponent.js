eadfb049a1fdf1c5f5f9e33977c110e05d3b0669
1Dire <d01038145300@nate.com>
2023-11-09 00:27:11 +0900

    페이드인 에니메이션 추가

--- src/component/layoutComponent.js
+++ src/component/layoutComponent.js
@@ -1,52 +1,83 @@
 import React, { useState, useEffect } from "react";
-
-import { CSSTransition } from 'react-transition-group';
 import styles from "style/layoutComponent.module.css";
 
 const LayoutComponent = () => {
-
-  const [images, setImages] = useState([]); // 이미지들을 저장하는 state
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    // 이미지 데이터를 가져오는 비동기 함수 (예: API 호출)
     const fetchImages = async () => {
       try {
-        const response = []; // 이미지를 가져오는 API 호출이나 파일 경로로 변경해야 합니다.
-        let addImgCount = 8;
+        const response = [];
+        let addImgCount = 7;
         for (let i = 1; i <= addImgCount; i++) {
           let num = i < 10 ? "0" + String(i) : i;
           response.push({
-            src: "img" + num + ".jpg",
-            alt: "img" + num,
+            src: `img${num}.jpg`,
+            alt: `img${num}`,
           });
         }
-        
-        setImages(response); // 이미지 데이터를 state에 저장
-        
+
+        setImages(response);
       } catch (error) {
         console.error("Error fetching images:", error);
       }
     };
-    fetchImages(); // 이미지 데이터 가져오기
- 
+    fetchImages();
   }, []);
-  console.log(images)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(
+        (entry) => {
+          entry.target.classList.toggle(styles.show, entry.isIntersecting); // 컨텐츠 보일때 show
+          // if (entry.isIntersecting) {
+          //   observer.unobserve(entry.target); // 이미 보인거 재실행 안하게
+          // }
+        },
+        { threshold: 1, rootMargin: "0" }
+      );
+    });
+    const lastCardObserver = new IntersectionObserver(entries =>{
+     const lastCard = entries[0]
+     lastCard.target.classList.toggle(styles.show, lastCard.isIntersecting);
+     if(!lastCard.isIntersecting) return
+     loadNewCards()
+     lastCardObserver.observe(document.getElementById(`image-${images.length}`))
+    },{})
+
+
+    function loadNewCards (){//
+      //마지막카드
+      console.log('last')
+    } 
+    const lastCardElement = document.getElementById(`image-${images.length}`);
+    if (lastCardElement) {
+      lastCardObserver.observe(lastCardElement);
+    }
+    images.forEach((image, index) => {
+      const imageRef = document.getElementById(`image-${index}`);
+      if (imageRef) {
+        observer.observe(imageRef);
+      }
+      
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [images]);
+
   return (
-    <>
-      <div className={styles.container}>
+    <div className={styles.container}>
       {images.map((image, index) => (
-        <CSSTransition
-        >
-        <div className={styles.box}>
+        <div className={styles.box} key={index} id={`image-${index + 1}`}>
           <img
             src={`${process.env.PUBLIC_URL}/images/${image.src}`}
-            alt={`${image.alt}`}
-          ></img>
+            alt={image.alt}
+          />
         </div>
-        </CSSTransition>
       ))}
-      </div>
-    </>
+    </div>
   );
 };
 
